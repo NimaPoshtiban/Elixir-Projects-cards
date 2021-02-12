b38ecf7beb6ed4503c2b00a5062918c04df1190a
Nima Poshtiban <poshtibannima@gmail.com>
2021-02-12 20:18:58 +0330

    Create_hand function added

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,20 @@
 defmodule Cards do
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five"]
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
     suits = ["Spades", "Clubs", "Hearts", "Diamond"]
 
     for suit <- suits, value <- values do
@@ -16,7 +30,25 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
-  def deal(deck,hand_size) do
-    Enum.split(deck,hand_size)
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does'nt exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
